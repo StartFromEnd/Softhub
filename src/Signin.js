68a398a1cd94eff24743c12fa761142549d55fc3
StartFromEnd <parkethan1210@gmail.com>
2024-01-01 11:16:28 +0000

    test2

--- src/Signin.js
+++ src/Signin.js
@@ -40,7 +40,7 @@ class Signin extends Component {
                     signinPassword: `${password}`
                 }),
             })
-            .then((response) => {console.log(response.json().Object);});
+            .then((response) => {console.log(response.json().ok);});
         }
     }
     render() {
