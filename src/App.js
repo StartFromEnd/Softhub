a7413341d771d2d0836b5c7f4952dd095770a930
박이든 <parkethan1210@gmail.com>
2024-01-03 05:54:25 +0000

    fix

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ class App extends Component {
             secure: true,
             httpOnly: true,
         });
-        this.CheckCookie;
+        this.CheckCookie();
     };
 
     CheckCookie = () => {
