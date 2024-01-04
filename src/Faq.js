748b50d19572c3de2eda5af83f33036a500e6b9d
박이든 <parkethan1210@gmail.com>
2024-01-04 09:59:34 +0000

    fix

--- src/Faq.js
+++ src/Faq.js
@@ -4,11 +4,22 @@ import logo from './logo.svg';
 import './App.css';
 
 class Faq extends Component {
+    
+    constructor(props) {
+        super(props);
+        this.state = {
+            faqNum: 0,
+            nowFaqNum: 1,
+            faqList: [],
+        };
+    }
+    
     render() {
         return (
             <div className="all">
                 <section className="cover-fixed-nav-section"></section>
                 <section className="faq-section container">문의하기</section>
+                <section className="faq-list-section container"></section>
                 <section className="faq-search-section container">
                     <div class="input-group mb-3 search">
                         <input
@@ -23,10 +34,69 @@ class Faq extends Component {
                         </button>
                     </div>
                 </section>
-                <section className="faq-list-section container"></section>
+                <section className="mt-3 container myfaq-search-section">
+                    <div className="container myfaq-write-button">
+                        <button className="btn btn-primary" type="button">글쓰기</button>
+                    </div>
+                    <div className="container myfaq-search-buttons">
+                        <button type="button">&lt;</button>
+                        <button type="button">{this.state.nowFaqNum}</button>
+                        <button
+                            type="button"
+                            style={
+                                this.state.faqNum > this.state.nowFaqNum * 10
+                                    ? myfaqStyles.dYes
+                                    : myfaqStyles.dNone
+                            }
+                        >
+                            {this.state.nowFaqNum + 1}
+                        </button>
+                        <button
+                            type="button"
+                            style={
+                                this.state.faqNum > this.state.nowFaqNum * 20
+                                    ? myfaqStyles.dYes
+                                    : myfaqStyles.dNone
+                            }
+                        >
+                            {this.state.nowFaqNum + 2}
+                        </button>
+                        <button
+                            type="button"
+                            style={
+                                this.state.faqNum > this.state.nowFaqNum * 30
+                                    ? myfaqStyles.dYes
+                                    : myfaqStyles.dNone
+                            }
+                        >
+                            {this.state.nowFaqNum + 3}
+                        </button>
+                        <button
+                            type="button"
+                            style={
+                                this.state.faqNum > this.state.nowFaqNum * 40
+                                    ? myfaqStyles.dYes
+                                    : myfaqStyles.dNone
+                            }
+                        >
+                            {this.state.nowFaqNum + 4}
+                        </button>
+                        <button type="button">&gt;</button>
+                    </div>
+                </section>
             </div>
         );
     }
 }
+
+const myfaqStyles = {
+    dNone: {
+        display: 'none',
+    },
+    dYes: {
+        display: 'inline-block',
+    },
+};
+
 
 export default Faq;
