b8febc416ec02806acbec3be1dcd24ba1b581489
박이든 <parkethan1210@gmail.com>
2024-01-04 06:54:18 +0000

    fix

--- src/Profil.js
+++ src/Profil.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 import cookie from 'react-cookies';
 import logo from './logo.svg';
 import './App.css';
@@ -81,6 +82,7 @@ class Profil extends Component {
                         </li>
                     </ul>
                 </section>
+                <Outlet />
             </div>
         );
     }
