dabaec817be903172faa749cbc53e74c3b556198
박이든 <parkethan1210@gmail.com>
2024-01-04 04:43:20 +0000

    fix

---
+++ src/Announce.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom';
+import logo from './logo.svg';
+import './App.css';
+
+class Announce extends Component {
+    render() {
+        return (
+            <div className="all">
+                <section className="announcement-section container">공지사항</section>
+                <section className="announcement-list-section container"></section>
+                <section className="announcement-search-section container">
+                    <div class="input-group mb-3">
+                        <input
+                            type="text"
+                            class="form-control"
+                            placeholder="글 제목"
+                            aria-label="Recipient's username"
+                            aria-describedby="button-addon2"
+                        ></input>
+                        <button class="btn btn-outline-secondary" type="button" id="button-addon2">
+                            검색
+                        </button>
+                    </div>
+                </section>
+            </div>
+        );
+    }
+}
+
+export default Announce;
