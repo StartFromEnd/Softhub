979420671e66034504e71eae2a9734c97308d891
박이든 <parkethan1210@gmail.com>
2024-01-07 09:19:55 +0000

    fix

--- src/Profils/Profil.js
+++ src/Profils/Profil.js
@@ -41,28 +41,38 @@ class Profil extends React.Component {
         return (
             <div>
                 <section className="profil-form-wrap container">
-                    <p className="one">{this.state.nickname}</p>
-                    <p className="one">{this.state.position}</p>
-                    <p className="one">{this.state.email}</p>
-                    <p className="one">{this.state.bankAccount}</p>
-                    <hr></hr>
-                    <p className="half">이메일은 사용자 식별정보이므로 변경이 불가합니다.</p>
-                    <p className="half">닉네임 변경을 원할 시 관리자에게 개인 문의하여 주십시오.</p>
-                    <p className="half">
-                        공인 혹은 상표가 있는 단체는 관리자의 판단하에 √ 표시를 부여받을 수
-                        있습니다.
-                    </p>
+                    <section className="profil-form-wrap-margin container">
+                        <p className="one">{this.state.nickname}</p>
+                        <p className="one">{this.state.position}</p>
+                        <p className="one">{this.state.email}</p>
+                        <p className="one">{this.state.bankAccount}</p>
+                        <hr></hr>
+                        <p className="half">이메일은 사용자 식별정보이므로 변경이 불가합니다.</p>
+                        <p className="half">
+                            닉네임 변경을 원할 시 관리자에게 개인 문의하여 주십시오.
+                        </p>
+                        <p className="half">
+                            공인 혹은 상표가 있는 단체는 관리자의 판단하에 √ 표시를 부여받을 수
+                            있습니다.
+                        </p>
+                    </section>
                 </section>
                 <section className="profil-routes container">
                     <ul className="nav justify-content-center">
                         <li className="nav-item">
-                            <NavLink to='/profil/mySupport' className='nav-link'>내 후원목록</NavLink>
+                            <NavLink to="/profil/mySupport" className="nav-link">
+                                내 후원목록
+                            </NavLink>
                         </li>
                         <li className="nav-item">
-                            <NavLink to='/profil/myOrder' className='nav-link'>내 주문목록</NavLink>
+                            <NavLink to="/profil/myOrder" className="nav-link">
+                                내 주문목록
+                            </NavLink>
                         </li>
                         <li className="nav-item">
-                            <NavLink to='/profil/myFaq' className='nav-link'>개인 문의하기</NavLink>
+                            <NavLink to="/profil/myFaq" className="nav-link">
+                                개인 문의하기
+                            </NavLink>
                         </li>
                     </ul>
                     <Outlet />
