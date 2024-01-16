8e1a385dd444e55c1dd8dfabaa30d2df8b7d5931
박이든 <parkethan1210@gmail.com>
2024-01-16 09:42:20 +0000

    fix

--- src/Profils/Mysupports/Mysupportwrite.js
+++ src/Profils/Mysupports/Mysupportwrite.js
@@ -3,8 +3,7 @@ import { BrowserRouter, Routes, Route, NavLink, Outlet } from 'react-router-dom'
 import cookie from 'react-cookies';
 import '../../App.css';
 import * as common from '../../CommonFunctions.js';
-import { CKEditor } from '@ckeditor/ckeditor5-react';
-import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import { CKEditor } from 'ckeditor4-react';
 
 class Mysupportwrite extends React.Component {
     constructor(props){
@@ -74,25 +73,7 @@ class Mysupportwrite extends React.Component {
                         onChange={(event) => this.SaveGoal(event.target.value)}
                     ></input>
                 </div>
-                <CKEditor
-                    editor={ ClassicEditor }
-                    editors={ ClassicEditor }
-                    data="<p>Hello from CKEditor 5!</p>"
-                    onReady={ editor => {
-                        // You can store the "editor" and use when it is needed.
-                        console.log( 'Editor is ready to use!', editor );
-                    } }
-                    onChange={ ( event, editor ) => {
-                        const data = editor.getData();
-                        console.log( { event, editor, data } );
-                    } }
-                    onBlur={ ( event, editor ) => {
-                        console.log( 'Blur.', editor );
-                    } }
-                    onFocus={ ( event, editor ) => {
-                        console.log( 'Focus.', editor );
-                    } }
-                />
+                <CKEditor />
             </div>
         )
     }
