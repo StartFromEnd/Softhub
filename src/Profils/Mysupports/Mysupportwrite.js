7be3189f867e2ab3d447fefc05c8de56bb9f1664
박이든 <parkethan1210@gmail.com>
2024-01-16 09:30:02 +0000

    fix

--- src/Profils/Mysupports/Mysupportwrite.js
+++ src/Profils/Mysupports/Mysupportwrite.js
@@ -13,7 +13,6 @@ class Mysupportwrite extends React.Component {
             title: '',
             price: '',
             goal: '',
-            data: '',
         }
     }
     
@@ -26,10 +25,6 @@ class Mysupportwrite extends React.Component {
     SaveGoal(param){
         this.setState({ goal: param});
     }
-    SaveData(param){
-        this.setState({ data: param});
-    }
-    
     componentDidMount() {
         
     }
@@ -81,23 +76,21 @@ class Mysupportwrite extends React.Component {
                 </div>
                 <CKEditor
                     editor={ ClassicEditor }
-                    config={{
-                        placeholder: '내용을 입력하세요.',
-                    }}
-                    data = { this.state.data }
-                    onReady={ (editor) =>
+                    data="<p>Hello from CKEditor 5!</p>"
+                    onReady={ editor => {
                         // You can store the "editor" and use when it is needed.
-                        console.log( 'Editor is ready to use!', editor )
-                    }
-                    onChange = {(event, editor) =>
-                        this.SaveData(editor.getData())
-                    }
-                    onBlur={ ( event, editor ) =>
-                        console.log( 'Blur.', editor )
-                    }
-                    onFocus={ ( event, editor ) =>
-                        console.log( 'Focus.', editor )
-                    }
+                        console.log( 'Editor is ready to use!', editor );
+                    } }
+                    onChange={ ( event, editor ) => {
+                        const data = editor.getData();
+                        console.log( { event, editor, data } );
+                    } }
+                    onBlur={ ( event, editor ) => {
+                        console.log( 'Blur.', editor );
+                    } }
+                    onFocus={ ( event, editor ) => {
+                        console.log( 'Focus.', editor );
+                    } }
                 />
             </div>
         )
