d73ea9f8651735ed8bdb09ff946d030b64975234
박이든 <parkethan1210@gmail.com>
2024-01-17 09:53:57 +0000

    fix

--- src/Profils/Mysupports/Mysupportwrite.js
+++ src/Profils/Mysupports/Mysupportwrite.js
@@ -3,7 +3,7 @@ import { BrowserRouter, Routes, Route, NavLink, Outlet } from 'react-router-dom'
 import cookie from 'react-cookies';
 import '../../App.css';
 import * as common from '../../CommonFunctions.js';
-import CKEditor from '@ckeditor/ckeditor5-react';
+import {CKEditor} from '@ckeditor/ckeditor5-react';
 
 class Mysupportwrite extends React.Component {
     constructor(props) {
