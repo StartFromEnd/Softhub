993fa217af85078177099e0d97a8037154e268f9
박이든 <parkethan1210@gmail.com>
2024-01-22 06:43:07 +0000

    fix

--- src/Rendersupport.js
+++ src/Rendersupport.js
@@ -7,14 +7,14 @@ import CameraImages from './Images/camera.png';
 
 function Renderflexitems({index, link, image, title, price, percent}){
     return(
-        <div className='flexitems'>
+        <div className='flex-items'>
             <NavLink to={link} className='none-style-link'>
                 <img src={image[0] == 'null' ? CameraImages : 'https://storage.googleapis.com/softhub-image-storage/'+image[0]} alt='제품사진'></img>
                 <p className='bold one'>{title}</p>
                 <p className='bold half'>{`${price}원`}</p>
                 <p className='bold half'>후원 {100*percent}% 진행중</p>
                 <div className='border-round'>
-                    <div className='border-round bg-skyblue' width={`${100 * percent}%`}>
+                    <div className='border-round bg-skyblue' style={{width:`${100 * percent}%`}}>
                     </div>
                 </div>
             </NavLink>
