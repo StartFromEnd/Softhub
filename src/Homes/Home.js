44d4033cc5c97729f042d4bde6d9d2581fc84967
박이든 <parkethan1210@gmail.com>
2024-01-23 07:06:12 +0000

    fix

--- src/Homes/Home.js
+++ src/Homes/Home.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Routes, Route, NavLink } from 'react-router-dom';
 import cookie from 'react-cookies';
-import logo from './logo.svg';
+import logo from '../logo.svg';
 import '../App.css';
 import * as common from '../CommonFunctions.js';
 
