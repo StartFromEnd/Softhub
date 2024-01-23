2318cd8f18ae9324dd3f0fc00984699fb61d3077
박이든 <parkethan1210@gmail.com>
2024-01-23 07:56:41 +0000

    fix

--- src/Homes/Home.js
+++ src/Homes/Home.js
@@ -22,15 +22,15 @@ class Home extends React.Component {
                             <NavLink to='/main' className='none-style-link block font-1rem black'>FundHub</NavLink>
                         </div>
                         <div className='navbar-middle d-flex justify-content-between'>
-                            <NavLink to='/' className='none-style-link block font-0-75rem purple bold'>시작하기</NavLink>
-                            <NavLink to='/main' className='none-style-link block font-0-75rem gray bold'>홈</NavLink>
-                            <NavLink to='/announce' className='none-style-link block font-0-75rem gray bold'>공지사항</NavLink>
-                            <NavLink to='/faq' className='none-style-link block font-0-75rem gray bold'>문의하기</NavLink>
+                            <NavLink to='/' className='none-style-link block font-0-75rem purple'>시작하기</NavLink>
+                            <NavLink to='/main' className='none-style-link block font-0-75rem gray'>홈</NavLink>
+                            <NavLink to='/announce' className='none-style-link block font-0-75rem gray'>공지사항</NavLink>
+                            <NavLink to='/faq' className='none-style-link block font-0-75rem gray'>문의하기</NavLink>
                         </div>
                         <div className='navbar-right d-flex justify-content-end'>
-                            <NavLink to='/write' className='none-style-link block font-0-75rem gray bold'>펀드 게시하기</NavLink>
+                            <NavLink to='/write' className='none-style-link block font-0-75rem gray'>펀드 게시하기</NavLink>
                             <div className='navbar-right-button'>
-                                <NavLink to='/login' className='none-style-link block font-0-75rem white bold'>로그인</NavLink>
+                                <NavLink to='/login' className='none-style-link block font-0-75rem white'>로그인</NavLink>
                             </div>
                         </div>
                     </div>
