83f9eb8fbb24dc2dd276bf45cf0b09d15f8eca44
박이든 <parkethan1210@gmail.com>
2024-01-23 07:26:16 +0000

    fix

--- src/Homes/Home.js
+++ src/Homes/Home.js
@@ -19,13 +19,19 @@ class Home extends React.Component {
                 <section className='navbar'>
                     <div className='navbar-container container d-flex justify-content-between'>
                         <div className='navbar-left d-flex justify-content-start'>
-                            <NavLink to='/' className='none-style-link block'>FundHub</NavLink>
+                            <NavLink to='/main' className='none-style-link block font-1-5rem'>FundHub</NavLink>
                         </div>
                         <div className='navbar-middle d-flex justify-content-between'>
-                            
+                            <NavLink to='/' className='none-style-link block font-1rem blue'>시작하기</NavLink>
+                            <NavLink to='/main' className='none-style-link block font-1rem gray'>홈</NavLink>
+                            <NavLink to='/announce' className='none-style-link block font-1rem gray'>공지사항</NavLink>
+                            <NavLink to='/faq' className='none-style-link block font-1rem gray'>문의하기</NavLink>
                         </div>
                         <div className='navbar-right d-flex justify-content-end'>
-                            
+                            <NavLink to='/write' className='none-style-link block font-1rem gray'>펀드 게시하기</NavLink>
+                            <div className='navbar-right-button'>
+                                <NavLink to='/login' className='none-style-link block font-1rem white'>로그인</NavLink>
+                            </div>
                         </div>
                     </div>
                 </section>
