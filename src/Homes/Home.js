46a9dc7cf022100271bfddc291a771ef7fb1efcc
박이든 <parkethan1210@gmail.com>
2024-01-23 07:30:24 +0000

    fix

--- src/Homes/Home.js
+++ src/Homes/Home.js
@@ -19,18 +19,18 @@ class Home extends React.Component {
                 <section className='navbar'>
                     <div className='navbar-container container d-flex justify-content-between'>
                         <div className='navbar-left d-flex justify-content-start'>
-                            <NavLink to='/main' className='none-style-link block font-1-5rem'>FundHub</NavLink>
+                            <NavLink to='/main' className='none-style-link block font-1rem black'>FundHub</NavLink>
                         </div>
                         <div className='navbar-middle d-flex justify-content-between'>
-                            <NavLink to='/' className='none-style-link block font-1rem blue'>시작하기</NavLink>
-                            <NavLink to='/main' className='none-style-link block font-1rem gray'>홈</NavLink>
-                            <NavLink to='/announce' className='none-style-link block font-1rem gray'>공지사항</NavLink>
-                            <NavLink to='/faq' className='none-style-link block font-1rem gray'>문의하기</NavLink>
+                            <NavLink to='/' className='none-style-link block font-0-75rem blue'>시작하기</NavLink>
+                            <NavLink to='/main' className='none-style-link block font-0-75rem gray'>홈</NavLink>
+                            <NavLink to='/announce' className='none-style-link block font-0-75rem gray'>공지사항</NavLink>
+                            <NavLink to='/faq' className='none-style-link block font-0-75rem gray'>문의하기</NavLink>
                         </div>
                         <div className='navbar-right d-flex justify-content-end'>
-                            <NavLink to='/write' className='none-style-link block font-1rem gray'>펀드 게시하기</NavLink>
+                            <NavLink to='/write' className='none-style-link block font-0-75rem gray'>펀드 게시하기</NavLink>
                             <div className='navbar-right-button'>
-                                <NavLink to='/login' className='none-style-link block font-1rem white'>로그인</NavLink>
+                                <NavLink to='/login' className='none-style-link block font-0-75rem white'>로그인</NavLink>
                             </div>
                         </div>
                     </div>
